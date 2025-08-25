a16f4e255552e66df620bd94bd7bbf5e9dfb0392
AnusreeMP <anuspreeprakash730@gmail.com>
2025-08-25 19:02:17 +0530

    Added conditionalstatements program

---
+++ if and switch.js
@@ -0,0 +1,67 @@
+// let age=20;
+// if(age<18){
+//     console.log("You are a minor.");
+// }
+// else if(age>=18 && age<60){
+//     console.log("You are an adult.");
+
+// }
+// else{
+//     console.log("You are a senior citizen.");
+// }
+
+// let num=0;
+// if (isNaN(num))  {
+//     console.log("not a number");
+// }else if(num > 0) {
+//     console.log("positive");
+// }
+// else if(num < 0) {
+// console.log("negative");
+// }
+// else {
+//     console.log("zero");
+// }
+
+// switch
+// let mark = 75;
+// switch (true) {
+    // case mark >= 90:
+    //     console.log("A grade");
+    //     break;
+    // case mark >= 75:
+    //     console.log("B grade");
+    //     break;
+    // case mark >= 60:
+    //     console.log("c grade");
+    //     break;
+    // case mark >= 50:
+    //     console.log("D grade");
+    //     break;
+    // case mark < 50:
+    //     console.log("F grade");
+    //     break;
+    // default:
+    //     console.log("invalid mark");
+    //     break;
+// }
+
+let day =3;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    default:
+        console.log("Incorrect day");
+        break;        
+}
+
